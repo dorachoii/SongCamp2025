8de6cd81666b9ea0eb181be1097e0939a9dc6d18
Dora Choi <hellodoradora7@gmail.com>
2025-06-24 13:16:30 +0900

    fix: Change variables name (allgamenoteinfo -> noteSpawnQueue)

--- Assets/01.Scripts/NoteManager.cs
+++ Assets/01.Scripts/NoteManager.cs
@@ -9,19 +9,22 @@ public class NoteManager : MonoBehaviour
     int bpm = 60;
 
     public GameObject[] notePrefabs;
-    public List<Transform> spawnPos;
+    public List<Transform> spawnRails;
 
-    private List<GameNoteInfo> allGameNoteInfo = new List<GameNoteInfo>();
-    private List<GameNoteInfo>[] gameNoteInfo_Rails = new List<GameNoteInfo>[railCount];
+    // 대기열 느낌으로 이릅 바꾸기
+    private List<GameNoteInfo> noteSpawnQueue = new List<GameNoteInfo>();
+    // 레일별 대기열으로 이름 바꾸기
+    private List<GameNoteInfo>[] noteSpawnQueue_perRail = new List<GameNoteInfo>[railCount];
 
-    List<EJGameNote>[] gameNoteInstance_Rails = new List<EJGameNote>[railCount];
+    // 레입별 게임씬 인스턴스 
+    List<EJGameNote>[] spawnedNotes_perRail = new List<EJGameNote>[railCount];
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < gameNoteInstance_Rails.Length; i++)
+        for (int i = 0; i < spawnedNotes_perRail.Length; i++)
         {
-            gameNoteInstance_Rails[i] = new List<EJGameNote>();
+            spawnedNotes_perRail[i] = new List<EJGameNote>();
         }
 
         TestShortNotes();
@@ -32,17 +35,17 @@ public class NoteManager : MonoBehaviour
     {
         currTime += Time.deltaTime;
 
-        for (int i = 0; i < gameNoteInfo_Rails.Length; i++)
+        for (int i = 0; i < noteSpawnQueue_perRail.Length; i++)
         {
-            if (gameNoteInfo_Rails[i].Count > 0)
+            if (noteSpawnQueue_perRail[i].Count > 0)
             {
-                if (currTime >= gameNoteInfo_Rails[i][0].time / bpm)
+                if (currTime >= noteSpawnQueue_perRail[i][0].time / bpm)
                 {
-                    GameObject prefab = notePrefabs[gameNoteInfo_Rails[i][0].type];
-                    GameObject note = Instantiate(prefab, spawnPos[i].position + Vector3.forward * (-0.5f), prefab.transform.rotation);
+                    GameObject prefab = notePrefabs[noteSpawnQueue_perRail[i][0].type];
+                    GameObject note = Instantiate(prefab, spawnRails[i].position + Vector3.forward * (-0.5f), prefab.transform.rotation);
 
                     //note.transform.forward = spawnPos[0].transform.forward;
-                    note.transform.SetParent(spawnPos[i].transform);
+                    note.transform.SetParent(spawnRails[i].transform);
 
                     EJGameNote noteInstance = note.GetComponent<EJGameNote>();
                     noteInstantiate(i, noteInstance);
@@ -53,9 +56,9 @@ public class NoteManager : MonoBehaviour
 
     void noteInstantiate(int n, EJGameNote noteInstance)
     {
-        noteInstance.noteInfo = gameNoteInfo_Rails[n][0];
-        gameNoteInstance_Rails[n].Add(noteInstance);
-        gameNoteInfo_Rails[n].RemoveAt(0);
+        noteInstance.noteInfo = noteSpawnQueue_perRail[n][0];
+        spawnedNotes_perRail[n].Add(noteInstance);
+        noteSpawnQueue_perRail[n].RemoveAt(0);
     }
 
     void TestShortNotes()
@@ -66,41 +69,41 @@ public class NoteManager : MonoBehaviour
         info.railIdx = 0;
         info.type = (int)GameNoteType.SHORT;
         info.time = 1 * bpm;
-        allGameNoteInfo.Add(info);
+        noteSpawnQueue.Add(info);
 
         info.railIdx = 1;
         info.type = (int)GameNoteType.SHORT;
         info.time = 2 * bpm;
-        allGameNoteInfo.Add(info);
+        noteSpawnQueue.Add(info);
 
         info.railIdx = 2;
         info.type = (int)GameNoteType.SHORT;
         info.time = 3 * bpm;
-        allGameNoteInfo.Add(info);
+        noteSpawnQueue.Add(info);
 
         info.railIdx = 3;
         info.type = (int)GameNoteType.SHORT;
         info.time = 4 * bpm;
-        allGameNoteInfo.Add(info);
+        noteSpawnQueue.Add(info);
 
         info.railIdx = 4;
         info.type = (int)GameNoteType.SHORT;
         info.time = 5 * bpm;
-        allGameNoteInfo.Add(info);
+        noteSpawnQueue.Add(info);
 
         info.railIdx = 5;
         info.type = (int)GameNoteType.SHORT;
         info.time = 6 * bpm;
-        allGameNoteInfo.Add(info);
+        noteSpawnQueue.Add(info);
         
-        for (int i = 0; i < gameNoteInfo_Rails.Length; i++)
+        for (int i = 0; i < noteSpawnQueue_perRail.Length; i++)
         {
-            gameNoteInfo_Rails[i] = new List<GameNoteInfo>();
+            noteSpawnQueue_perRail[i] = new List<GameNoteInfo>();
         }
 
-        for (int i = 0; i < allGameNoteInfo.Count; i++)
+        for (int i = 0; i < noteSpawnQueue.Count; i++)
         {
-            gameNoteInfo_Rails[allGameNoteInfo[i].railIdx].Add(allGameNoteInfo[i]);
+            noteSpawnQueue_perRail[noteSpawnQueue[i].railIdx].Add(noteSpawnQueue[i]);
         }
     }
 }
